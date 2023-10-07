7ab68634cf8d814add6372a947fec1c925eec05d
Alejandro Matías Casco <alejandromatiascasco@gmail.com>
2023-10-07 13:30:04 -0300

    Creando un home

---
+++ grupo4_frontend/src/components/home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Home() {
+  // Lógica para cargar y mostrar noticias principales
+
+  return (
+    <div>
+      <h2>Noticias Destacadas</h2>
+      {/* Renderizar las noticias destacadas aquí */}
+    </div>
+  );
+}
+
+export default Home;
