1069f68e53e1ea60ba483509de534e43d92bea9d
Alejandro Matías Casco <alejandromatiascasco@gmail.com>
2023-10-08 20:43:37 -0300

    transformando en .jsx

---
+++ grupo4_frontend/src/components/home.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import imagen1 from './images/imagen_noticia1.jpg'
+import 'bootstrap/dist/css/bootstrap.css';
+
+function Home() {
+  // Lógica para cargar y mostrar noticias principales
+
+  return (
+    <div className='container'>
+      <h2>Lo que está pasando</h2>
+      <div className="row justify-content-center text-right">
+        <div className="col-lg-9">
+          <img src={imagen1} className='img-thumbnail' alt="Imagen de la noticia" />
+          <h2>Título de la Noticia</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque molestie enim sed tincidunt ornare. Proin vehicula turpis a odio euismod, id venenatis augue molestie<a href="#"> ... Leer más</a></p>
+        </div>
+      </div>
+      <div className="row justify-content-around news_container">
+        <div className='col-lg-3'>
+          <img src={imagen1} className='img-thumbnail' alt="Imagen de la noticia" />
+          <h2>Título de la Noticia</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque molestie enim sed tincidunt ornare. Proin vehicula turpis a odio euismod, id venenatis augue molestie<a href="#"> ... Leer más</a></p>
+        </div>
+        <div className='col-lg-3'>
+          <img src={imagen1} className='img-thumbnail' alt="Imagen de la noticia" />
+          <h2>Título de la Noticia</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque molestie enim sed tincidunt ornare. Proin vehicula turpis a odio euismod, id venenatis augue molestie<a href="#"> ... Leer más</a></p>
+        </div>
+        <div className='col-lg-3'>
+          <img src={imagen1} className='img-thumbnail' alt="Imagen de la noticia" />
+          <h2>Título de la Noticia</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque molestie enim sed tincidunt ornare. Proin vehicula turpis a odio euismod, id venenatis augue molestie<a href="#"> ... Leer más</a></p>
+        </div>
+      </div>
+      <div className="row justify-content-around">
+        <div className="col-lg-5">
+          <img src={imagen1} className='img-thumbnail' alt="Imagen de la noticia" />
+          <h2>Título de la Noticia</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque molestie enim sed tincidunt ornare. Proin vehicula turpis a odio euismod, id venenatis augue molestie<a href="#"> ... Leer más</a>
+          </p>
+        </div>
+        <div className="col-lg-5">
+          <img src={imagen1} className='img-thumbnail' alt="Imagen de la noticia" />
+          <h2>Título de la Noticia</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque molestie enim sed tincidunt ornare. Proin vehicula turpis a odio euismod, id venenatis augue molestie<a href="#"> ... Leer más</a></p>
+        </div>
+      </div>
+      <div className="row justify-content-around news_container">
+        <div className='col-lg-3'>
+          <img src={imagen1} className='img-thumbnail' alt="Imagen de la noticia" />
+          <h2>Título de la Noticia</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque molestie enim sed tincidunt ornare. Proin vehicula turpis a odio euismod, id venenatis augue molestie<a href="#"> ... Leer más</a></p>
+        </div>
+        <div className='col-lg-3'>
+          <img src={imagen1} className='img-thumbnail' alt="Imagen de la noticia" />
+          <h2>Título de la Noticia</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque molestie enim sed tincidunt ornare. Proin vehicula turpis a odio euismod, id venenatis augue molestie<a href="#"> ... Leer más</a></p>
+        </div>
+        <div className='col-lg-3'>
+          <img src={imagen1} className='img-thumbnail' alt="Imagen de la noticia" />
+          <h2>Título de la Noticia</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque molestie enim sed tincidunt ornare. Proin vehicula turpis a odio euismod, id venenatis augue molestie<a href="#"> ... Leer más</a></p>
+        </div>
+      </div>
+
+    </div>
+
+  );
+}
+
+export default Home;
